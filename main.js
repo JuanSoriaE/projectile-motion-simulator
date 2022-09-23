ba4d34115428939f30a37a0d9b6600e2eb54921a
JuanSoriaE <jsoriae1700@alumno.ipn.mx>
2022-09-23 16:21:49 -0500

    Added air resistance force, and cleaned up code

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const angle_input = document.getElementById('initial-angle');
 const velocity_input = document.getElementById('initial-vel');
 const g_input = document.getElementById('initial-g');
 const angle_span = document.getElementById('span-initial-angle');
-const velocity_span = document.getElementById('span-initial-vel');
-const g_span = document.getElementById('span-initial-g');
+const x_velocity_span = document.getElementById('x_vel');
+const y_velocity_span = document.getElementById('y_vel');
+const air_resistance_check = document.getElementById('air-resistance');
 
 // Default values
 let play = false;
@@ -16,16 +17,28 @@ let y_component = 0.7;
 let g = -9.81;
 const time_step = 0.03; // Have to be in seconds
 
+// Values for air drag force
+const k = 0.35;
+
 let vel_x = initial_vel * x_component;
 let vel_y = initial_vel * y_component;
 let x = 0, y = 0;
+
+// Sum of forces
+let x_sum = 0;
+let y_sum = 0;
+let sum_air_helper;
 
 function update() {
   x = x + vel_x*time_step;
   y = y + vel_y*time_step;
 
-  vel_x = vel_x;
-  vel_y = vel_y + g*time_step;
+  // Sum of forces
+  x_sum = sum_air_helper * vel_x**2;
+  y_sum = g + (sum_air_helper * vel_y**2);
+
+  vel_x = vel_x + x_sum*time_step;
+  vel_y = vel_y + y_sum*time_step;
 }
 
 function sleep(ms) {
@@ -37,36 +50,33 @@ function updatePosition() {
   projectile.style.bottom = `${y}px`;
 }
 
-function setData() {
-  // Get input data
-  initial_angle = Number(angle_input.value);
-  initial_vel = Number(velocity_input.value);
-  g = Number(g_input.value);
-
+function displayData() {
   angle_span.innerText = initial_angle;
-  velocity_span.innerHTML = initial_vel;
-  g_span.innerText = g;
-
-  x_component = Math.cos(initial_angle * Math.PI / 180).toFixed(2);
-  y_component = Math.sin(initial_angle * Math.PI / 180).toFixed(2);
-  vel_x = initial_vel * x_component;
-  vel_y = initial_vel * y_component;
+  x_velocity_span.innerHTML = vel_x.toFixed(2);
+  y_velocity_span.innerText = vel_y.toFixed(2);
 }
 
-function restoreValues() {
-  initial_angle = 45;
-  initial_vel = 30;
-  x_component = Math.cos(initial_angle * Math.PI / 180).toFixed(2);
-  y_component = Math.sin(initial_angle * Math.PI / 180).toFixed(2);
-
+function setData() {
+  // Get input data
+  initial_angle = Number(angle_input.value) || 45;
+  initial_vel = Number(velocity_input.value) || 30;
+  g = Number(g_input.value) || -9.8;
+  
+  x_component = Math.cos(initial_angle * Math.PI / 180).toFixed(2) || 0.7;
+  y_component = Math.sin(initial_angle * Math.PI / 180).toFixed(2) || 0.7;
   vel_x = initial_vel * x_component;
   vel_y = initial_vel * y_component;
-  x = 0, y = 0;
+  x = 0;
+  y = 0;
+
+  if (air_resistance_check.checked) sum_air_helper = -k/10;
+  else sum_air_helper = 0;
+  
+  displayData();
 }
 
 function stop() {
   play = false;
-  restoreValues();
   setData();
   updatePosition();
 }
@@ -75,18 +85,53 @@ async function run() {
   let i = 0;
   play = true;
   let width = ground.clientWidth - 15;
-  restoreValues();
+
   setData();
   updatePosition();
+
   while (y >= 0 && x <= width && i < 1000 && play) {
-    update();
     await sleep(time_step*1000);
+    update();
     updatePosition();
+    displayData();
     i++;
   }
   play = false;
 }
 
+// Ground Markers
+let markers_counter = 0;
+
+const deleteMarker = e => {
+  e.preventDefault();
+  const marker = document.getElementById(e.target.id);
+  marker.remove();
+};
+
+ground.addEventListener('click', e => {
+  const ground_rect = ground.getBoundingClientRect();
+  const i = document.createElement('i');
+  i.onauxclick = e => deleteMarker(e);
+  i.classList.add('marker');
+  i.style.left = `${e.clientX - ground_rect.x - 5}px`;
+  i.style.top = `${e.clientY - ground_rect.y - 5}px`;
+  
+  markers_counter++;
+  i.id = `marker-${markers_counter}`;
+  
+  ground.appendChild(i);
+});
+
+const clearMarkers = () => {
+  let marker;
+  for (let i = 1; i <= markers_counter; i++) {
+    marker = document.getElementById(`marker-${i}`);
+    if (!marker) continue;
+    marker.remove();
+  }
+};
+
+// MAIN
 function main() {
   setData();
 }
